b1de117899b7bba25949adff232b31aa230efb9a
Xinyu Wu <wxyxinyu@gmail.com>
2016-04-17 23:51:50 -0400

    stable roommate matching

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from rand import get_rand
 from dist import *
 from algos import *
+from stable import *
 import sys
 
-fout=open('results.csv','w')
-# fout=sys.stdout
+# fout=open('results.csv','w')
+fout=sys.stdout
 
-instlist=get_rand(100,12,'abcde')
+instlist=get_rand(100,12,'42')
 for inst in instlist:
 	precomp=prepare(inst)
 	# fout.write("%f\n" % sum(precomp[0]))
-	fout.write("%f\t%f\t%f\n" % (best(inst,4,precomp),km(inst,4,precomp),sum(precomp[0])))
+	fout.write("%f\t%f\t%f\n" % (best(inst,2,precomp),stable(inst,2,precomp),sum(precomp[0])))
 fout.close()
 
 # fout=open('2results.csv','w')
