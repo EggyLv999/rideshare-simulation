4edc602364d7864e703819038cebc2e3ddb5dba4
Edgar Chen <eggylv999@gmail.com>
2016-04-17 21:40:43 -0700

    k=4 version

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ instlist=get_rand(100,12,'42')
 for inst in instlist:
 	precomp=prepare(inst)
 	# fout.write("%f\n" % sum(precomp[0]))
-	fout.write("%f\t%f\t%f\n" % (best(inst,2,precomp),stable(inst,2,precomp),sum(precomp[0])))
+	s="%f\t%f\t%f\n" % (best(inst,4,precomp),km(inst,4,precomp),sum(precomp[0]))
+	print s
+	fout.write(s)
 fout.close()
 
 # fout=open('2results.csv','w')
